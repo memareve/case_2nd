1de2538da63fd5fca2f761dd158c32a56b3c9159
memareve <yqqN87cjbeER>
2020-11-09 10:22:26 +0700

    calculating_annual_income

--- main.py
+++ main.py
@@ -2,3 +2,14 @@
 # Developers: Marinkin O. ( ),
 # Seledtsov A. ( ),
 # Evdischenko M. ( ).
+
+# calculating_annual_income
+# string constants
+name_month = ['JAN', 'FAB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC']
+QUESTION = 'Your income in'
+annual_income = 0
+for month in range(12):
+    print('{} {}:'.format(QUESTION, name_month[month], end=''))
+    income = float(input())
+    annual_income += income
+print(annual_income)
