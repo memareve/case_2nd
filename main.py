ca6d1975b59fa6b7fd7176d03b330112bc6d0cb0
memareve <yqqN87cjbeER>
2020-11-09 10:41:05 +0700

    alone

--- main.py
+++ main.py
@@ -4,7 +4,6 @@
 # Evdischenko M. ( ).
 
 # calculating_annual_income
-# string constants
 name_month = ['JAN', 'FAB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC']
 QUESTION = 'Ваш доход в'
 annual_income = 0
@@ -12,4 +11,33 @@ for month in range(12):
     print('{} {}:'.format(QUESTION, name_month[month], end=''))
     income = float(input())
     annual_income += income
-print(annual_income)
+
+# main questions
+subject_of_taxation = input('Субъект налогообложения:').lower().replace(' ', '')
+tax_deduction = input('Сумма налогового вычета: ')
+
+annual_income = annual_income - tax_deduction
+
+if subject_of_taxation == 'одинсубъект':
+    if 0 <= annual_income <= 9075:
+
+    elif 9076 <= annual_income <= 36900:
+
+    elif 36901 <= annual_income <= 89350:
+
+    elif 89351 <= annual_income <= 186350:
+
+    elif 186351 <= annual_income <= 405100:
+
+    elif 405101 <= annual_income <= 406750:
+
+    elif annual_income >= 406751:
+
+
+elif subject_of_taxation == 'супружескаяпара':
+
+
+elif subject_of_taxation == 'родитель-одиночка':
+
+
+
