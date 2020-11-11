fe809dfc2048f982ab9221b1b39574611b9125e9
memareve <yqqN87cjbeER>
2020-11-11 22:49:56 +0700

    fix stage contants

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ for month in range(12):
     print('{} {}:'.format(QUESTION, name_month[month]), end=' ')
     income = float(input())
     annual_income += income
+print('Ваш сумарный доход:', annual_income)
 
 # main questions
 tax_deduction = float(input('Сумма налогового вычета: '))
@@ -24,46 +25,46 @@ annual_income = annual_income - tax_deduction
 
 # constants for the stage of the tax
 if subject_of_taxation == 'одинсубъект':
-    second = 9076
-    third = 36901
-    fourth = 89351
-    fifth = 186351
-    seventh = 406751
+    second = 9075
+    third = 36900
+    fourth = 89350
+    fifth = 186350
+    seventh = 406750
 elif subject_of_taxation == 'супружескаяпара':
-    second = 18151
-    third = 73801
-    fourth = 148851
-    fifth = 226851
-    seventh = 457601
+    second = 18150
+    third = 73800
+    fourth = 148850
+    fifth = 226850
+    seventh = 457600
 elif subject_of_taxation == 'родитель-одиночка' or subject_of_taxation == 'родительодиночка':
-    second = 12951
-    third = 49401
-    fourth = 127551
-    fifth = 206601
-    seventh = 432201
+    second = 12950
+    third = 49400
+    fourth = 127550
+    fifth = 206600
+    seventh = 432200
 
 # main
-if 0 <= annual_income <= (second - 1):
-    tax = 0.10 * (annual_income - 0)
+if 0 <= annual_income <= second:
+    tax = 0.10 * annual_income
     print('Сумма годового налога составит: ', tax)
-elif second <= annual_income <= (third - 1):
-    tax = 0.10 * (second - 0) + 0.15 * (annual_income - second)
+elif second < annual_income <= third:
+    tax = 0.10 * second + 0.15 * (annual_income - second)
     print('Сумма годового налога составит: ', tax)
-elif third <= annual_income <= (fourth - 1):
-    tax = 0.10 * (second - 0) + 0.15 * (third - second) + 0.25 * (annual_income - third)
+elif third < annual_income <= fourth:
+    tax = 0.10 * second + 0.15 * (third - second) + 0.25 * (annual_income - third)
     print('Сумма годового налога составит: ', tax)
-elif fourth <= annual_income <= (fifth - 1):
-    tax = 0.10 * (second - 0) + 0.15 * (third - second) + 0.25 * (fourth - third) + 0.28 * (annual_income - fourth)
+elif fourth < annual_income <= fifth:
+    tax = 0.10 * second + 0.15 * (third - second) + 0.25 * (fourth - third) + 0.28 * (annual_income - fourth)
     print('Сумма годового налога составит: ', tax)
-elif fifth <= annual_income <= 405100:
-    tax = 0.10 * (second - 0) + 0.15 * (third - second) + 0.25 * (fourth - third) + 0.28 * (fifth - fourth) + 0.33 * (
+elif fifth < annual_income <= 405100:
+    tax = 0.10 * second + 0.15 * (third - second) + 0.25 * (fourth - third) + 0.28 * (fifth - fourth) + 0.33 * (
             annual_income - fifth)
     print('Сумма годового налога составит: ', tax)
-elif 405101 <= annual_income <= (seventh - 1):
-    tax = 0.10 * (second - 0) + 0.15 * (third - second) + 0.25 * (fourth - third) + 0.28 * (fifth - fourth) + 0.33 * (
+elif 405101 < annual_income <= seventh:
+    tax = 0.10 * second + 0.15 * (third - second) + 0.25 * (fourth - third) + 0.28 * (fifth - fourth) + 0.33 * (
                     405101 - fifth) + 0.35 * (annual_income - 405101)
     print('Сумма годового налога составит: ', tax)
-elif seventh <= annual_income:
-    tax = 0.10 * (second - 0) + 0.15 * (third - second) + 0.25 * (fourth - third) + 0.28 * (fifth - fourth) + 0.33 * (
+elif seventh < annual_income:
+    tax = 0.10 * second + 0.15 * (third - second) + 0.25 * (fourth - third) + 0.28 * (fifth - fourth) + 0.33 * (
                     405101 - fifth) + 0.35 * (seventh - 405101) + 0.396 * (annual_income - seventh)
     print('Сумма годового налога составит: ', tax)
